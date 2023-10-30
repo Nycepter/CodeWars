dc30f8f25e6abf4a0bf91e2357b8402c6c3ae1ac
Aaron Jenkins <97716896+Nycepter@users.noreply.github.com>
2023-10-30 03:37:41 -0500

    Update Even or Odd.py

--- Python/Even or Odd.py
+++ Python/Even or Odd.py
@@ -1,7 +1,18 @@
 # Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 def even_or_odd(number):
+    """
+This function takes a number as input and returns whether it is even or odd.
+
+Args:
+    number (int or str): The number to be checked. If a string is provided, it will be converted to an integer.
+
+Returns:
+    str: "Even" if the number is even, "Odd" if the number is odd.
+"""
     if int(number) % 2 == 0:
+        # If the number is divisible by 2, it is even and "Even" is returned.
         return ("Even")
     else:
+        # If the number is not divisible by 2, it is odd and "Odd" is returned.
         return ("Odd")
